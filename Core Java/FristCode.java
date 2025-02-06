ab78b643b241dfb12d5cd691adccf73b9be36ffa
gangadhar195 <gangadhars9545@gmail.com>
2025-02-06 11:55:02 +0530

    java

---
+++ Core Java/FristCode.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FristCode{
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter Value A");
+       int a = sc.nextInt();
+       System.out.println("Enter Value B");
+
+       int b = sc.nextInt();
+        int sum = a+b;
+       System.out.println("Sum of A and B");
+
+        System.out.println(sum);
+
+    }
+}
