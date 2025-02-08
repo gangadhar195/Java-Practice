bc40f61bc71fb4cc88abeb133cb3ce569389209f
gangadhar195 <gangadhars9545@gmail.com>
2025-02-08 20:51:38 +0530

    Array Search

--- Core Java/Arrays.java
+++ Core Java/Arrays.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String args[]){
-       int marks[] = {88,90,95};
-       for(int i=0;i<marks.length;i++){
-        System.out.println(marks[i]);
-       }
+    //    int marks[] = {88,90,95};
+    //    for(int i=0;i<marks.length;i++){
+    //     System.out.println(marks[i]);
+    //    }
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Size of Array : ");
+    int n=sc.nextInt();
+    int number[] = new int[n];
+    System.out.println("Enter The Array Element : ");
+
+    for(int i=0;i<n;i++){
+         number[i] =sc.nextInt();
+    }
+
+    System.out.println("Enter the Found Element in Array : ");
+    int x = sc.nextInt();
+
+    for(int i=0;i<number.length;i++){
+        if(number[i]==x){
+            System.out.println("No Found At Index "+ i);
+        }
+    }
+
 
     }
 }
