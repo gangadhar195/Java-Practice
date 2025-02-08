bd44c07c5339c241033d22d90bce2d0919cee679
gangadhar195 <gangadhars9545@gmail.com>
2025-02-08 20:28:30 +0530

    Array

---
+++ Core Java/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String args[]){
+       int marks[] = {88,90,95};
+       for(int i=0;i<marks.length;i++){
+        System.out.println(marks[i]);
+       }
+
+    }
+}
