c4792666be598842d44307e407643adba5f1c62a
gangadhar195 <gangadhars9545@gmail.com>
2025-03-19 23:15:24 +0530

    Even Odd

--- Core Java/EvenOdd.java
+++ Core Java/EvenOdd.java
@@ -10,16 +10,27 @@ public class EvenOdd{
         // }
         //find greater no between two numbers
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Value No1 : ");
-        int n1=sc.nextInt();
-        System.out.print("Enter Value No2 : ");
-        int n2=sc.nextInt();
+        // System.out.print("Enter Value No1 : ");
+        // int n1=sc.nextInt();
+        // System.out.print("Enter Value No2 : ");
+        // int n2=sc.nextInt();
 
-        if(n1>n2){
-            System.out.println( "No1 is greater "+n1);
-        }
-        else{
-            System.out.println("No2 is greater "+n2);
-        }
+        // if(n1>n2){
+        //     System.out.println( "No1 is greater "+n1);
+        // }
+        // else{
+        //     System.out.println("No2 is greater "+n2);
+        // }
+
+        //Calculate Simple interest
+        System.out.print("Enter Principle Amount(P) : ");
+        int P = sc.nextInt();
+        System.out.print("Enter Rate of Interest (R) : ");
+        double R =sc.nextInt();
+        System.out.print("Enter the year (Y) : ");
+        double Y =sc.nextInt();
+
+        double cal = ((P*R*Y)/100);
+        System.out.println("Total Interest Amount Is : "+ cal);
     }
 }
