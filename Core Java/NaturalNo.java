1d38e80a399466a825e76423371e89ec45d9cb25
gangadhar195 <gangadhars9545@gmail.com>
2025-03-21 23:36:23 +0530

    Sum of Natural No

--- Core Java/NaturalNo.java
+++ Core Java/NaturalNo.java
@@ -1,5 +1,11 @@
 public class NaturalNo {
     public static void main(String args[]){
-        
+        int n=10;
+        int i=1;
+        int sum =0 ;
+        while(i<=n){
+            sum = sum + i;
+            System.out.println("Sum of 10 Natural : " + sum);
+        }
     }
 }
