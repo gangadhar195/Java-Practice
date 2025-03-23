16085f632960021f2d64cbf5f8fe23523fe299b0
gangadhar195 <gangadhars9545@gmail.com>
2025-03-23 08:27:35 +0530

    Type Conversion

--- Core Java/TypeConversion.java
+++ Core Java/TypeConversion.java
@@ -4,15 +4,25 @@ public class TypeConversion {
         //int to float Conversion
         int a = 20;
         float b = a;
-        System.out.println(b);
+        System.out.println("int to float : "+b);
         // float to int Conversion 
         float number1= 89.99f;
         int number2 = (int)number1;
-        System.out.println(number2);
+        // System.out.println(number2);
+        System.out.println("float :to int "+number2);
 
         // Char to int conversion
         char ch = 'a';
         int number = ch;
-        System.out.println(number);
+        System.out.println("char to int concersion : "+number);
+
+        // java automatically promotes each byte ,short char operand to int when evaulting expression
+        char ch1 = 'a';
+        char ch2 = 'b';
+        System.out.println((int)ch1);
+        System.out.println((int)ch2);
+
+        System.out.println(ch2-ch1);
+
     }
 }
