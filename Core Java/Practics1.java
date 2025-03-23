f42ddcacb2e27f74df406d230c29366d44a4f3f4
gangadhar195 <gangadhars9545@gmail.com>
2025-03-23 10:39:25 +0530

    Questions Pratics1

--- Core Java/Practics1.java
+++ Core Java/Practics1.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 public class Practics1 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter A : ");
-        int a =sc.nextInt();
-        System.out.print("Enter B : ");
-        int b =sc.nextInt();
-        System.out.print("Enter C : ");
-        int c =sc.nextInt();
+        //Question 1
+        // System.out.print("Enter A : ");
+        // int a =sc.nextInt();
+        // System.out.print("Enter B : ");
+        // int b =sc.nextInt();
+        // System.out.print("Enter C : ");
+        // int c =sc.nextInt();
 
-        int averge = (a + b +c)/3;
-        System.out.println("Average of 3 No : "+averge);
+        // int averge = (a + b +c)/3;
+        // System.out.println("Average of 3 No : "+averge);
+
+        //Question 2
+        System.out.print("Enter the Vaule of square : ");
+        int square = sc.nextInt();
+        int Asquare = (square *square);
+        System.out.println("The area of a square is : "+ Asquare);
+
     }
 }
