9c7eb1b87626edeea155d0d7045d147ba4e81931
gangadhar195 <gangadhars9545@gmail.com>
2025-03-23 08:02:46 +0530

    Type Conversion

---
+++ Core Java/TypeConversion.java
@@ -0,0 +1,11 @@
+public class TypeConversion {
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        int a = 20;
+        float b = a;
+        System.out.println(b);
+        float number1= 89.99f;
+        int number2 = (int)number1;
+        System.out.println(number2);
+    }
+}
