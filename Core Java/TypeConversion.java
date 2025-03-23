aea8b22f194e51bc1b251c694add83008b3d89fc
gangadhar195 <gangadhars9545@gmail.com>
2025-03-23 08:09:43 +0530

    Type Conversion

--- Core Java/TypeConversion.java
+++ Core Java/TypeConversion.java
@@ -1,11 +1,18 @@
 public class TypeConversion {
     public static void main(String args[]){
         // Scanner sc = new Scanner(System.in);
+        //int to float Conversion
         int a = 20;
         float b = a;
         System.out.println(b);
+        // float to int Conversion 
         float number1= 89.99f;
         int number2 = (int)number1;
         System.out.println(number2);
+
+        // Char to int conversion
+        char ch = 'a';
+        int number = ch;
+        System.out.println(number);
     }
 }
