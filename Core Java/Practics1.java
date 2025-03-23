8508c55b2ced663d716037693e1784686277f666
gangadhar195 <gangadhars9545@gmail.com>
2025-03-23 11:31:02 +0530

    Questions Pratics1

--- Core Java/Practics1.java
+++ Core Java/Practics1.java
@@ -15,10 +15,49 @@ public class Practics1 {
         // System.out.println("Average of 3 No : "+averge);
 
         //Question 2
-        System.out.print("Enter the Vaule of square : ");
-        int square = sc.nextInt();
-        int Asquare = (square *square);
-        System.out.println("The area of a square is : "+ Asquare);
+        // System.out.print("Enter the Vaule of square : ");
+        // int square = sc.nextInt();
+        // int Asquare = (square *square);
+        // System.out.println("The area of a square is : "+ Asquare);
+
+         //Question 3
+        //  System.out.print("Enter the Vaule of Pencil : ");
+        //  float p = sc.nextInt();
+        //  System.out.print("Enter the Vaule of Pen : ");
+        //  float pen = sc.nextInt();
+        //  System.out.print("Enter the Vaule of Eraser : ");
+        //  float e = sc.nextInt();
+        //     float total = p + pen + e ;
+        //     double gst = (total * 0.18);
+        //     double gstTotal = total + gst ;
+        //     System.out.println("_________________________________");
+        //     System.out.println("              Bill                ");
+        //     System.out.println("_________________________________");
+
+        //     System.out.println("Pencil :               " + p);
+        //     System.out.println("Pen :                  "  + pen);
+        //     System.out.println("Eraser :               " + e);
+        //     System.out.println("_________________________________");
+        //     System.out.println("Total without GST :    "+total);
+        //     System.out.println("GST 18%    :           "+(float)gst);
+        //     System.out.println("_________________________________");
+        //     System.out.println("Total Amount With GST : "+gstTotal);
+
+        // Question No 4
+
+        // byte b =4;
+        // char c = 'a';
+        // short s = 512;
+        // int i =1000;
+        // float f = 3.14f;
+        // double d = 99.9954;
+        // double result = (f * b) + ( i % c) -(d*s);
+        // System.out.println(result);
+
+        
+        
+
+
 
     }
 }
