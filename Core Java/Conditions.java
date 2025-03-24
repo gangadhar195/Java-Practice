5be002f8ad94c40466d6dc0a99502df20cd3ae4b
gangadhar195 <gangadhars9545@gmail.com>
2025-03-24 23:35:16 +0530

    Conditions

--- Core Java/Conditions.java
+++ Core Java/Conditions.java
@@ -7,11 +7,27 @@ public class Conditions {
         int a= 10;
         int b=++a;
         int c =a++;
-         c += a;
+        //  c += a;
          a+=b;
         System.out.println( "A is : "+a);
         System.out.println( "B is : "+b);
         System.out.println( "C is : "+c);
+        if(a>b){
+            if(a>c){
+                System.out.println("A is greater : "+a);
+            }
+            else{
+                System.out.println("C is Greater : "+c);
+            }
+        }
+        else{
+            if(b>c){
+                System.out.println("B is Greater : "+b);
+            }
+            else{
+                System.out.println("C is Greater : "+c);
+            }
+        }
 
 
     }
