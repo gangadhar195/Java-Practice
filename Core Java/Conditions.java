645cb6540397eeb1cfe628b8d8bc06b5135f5494
gangadhar195 <gangadhars9545@gmail.com>
2025-03-24 23:24:53 +0530

    Conditions

---
+++ Core Java/Conditions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Conditions {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a= 10;
+        int b=++a;
+        int c =a++;
+        System.out.println( "A is : "+a);
+        System.out.println( "B is : "+b);
+        System.out.println( "C is : "+c);
+
+
+    }
+}
