19edc4b4a985089bad4fc574a60af1677f60174c
gangadhar195 <gangadhars9545@gmail.com>
2025-03-26 09:26:17 +0530

    Practice3 Questions

--- Core Java/Practice3.java
+++ Core Java/Practice3.java
@@ -3,13 +3,26 @@ import java.util.*;
 public class Practice3 {
    public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a Number : ");
-    int n = sc.nextInt();
-    if(n>0){
-        System.out.println("Number is Positive : "+n);
+
+    //Question one
+    // System.out.print("Enter a Number : ");
+    // int n = sc.nextInt();
+    // if(n>0){
+    //     System.out.println("Number is Positive : "+n);
+    // }
+    // else{
+    //     System.out.println("Number is Negative :"+n);
+    // }
+
+    //Question Two
+    double temp = 103.5;
+
+    if(temp>100){
+        System.out.println("You have a fever");
     }
     else{
-        System.out.println("Number is Negative :"+n);
+        System.out.println("You don't have fever");
     }
+
    } 
 }
