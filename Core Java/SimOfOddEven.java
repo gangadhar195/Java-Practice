06cd60aebc4b303afdca3b8ef8e6e1f915afc8aa
gangadhar195 <gangadhars9545@gmail.com>
2025-03-27 22:33:02 +0530

    Sum of Even Odd no

--- Core Java/SimOfOddEven.java
+++ Core Java/SimOfOddEven.java
@@ -5,20 +5,25 @@ public class SimOfOddEven {
 
     int n = sc.nextInt();
 
-    int even=0;
-    int odd=0;
+    // int even=0;
+    // int odd=0;
 
-for(int i=1;i<n;i++){
-    int x=sc.nextInt();
-    if(x%2==0){
-        even+=x;
-    }
-    else{
-        odd+=x;
-    }
-}
-System.out.println("Sum of Even : "+even);
-System.out.println("Sum of odd : "+odd);
+// for(int i=1;i<n;i++){
+//     int x=sc.nextInt();
+//     if(x%2==0){
+//         even+=x;
+//     }
+//     else{
+//         odd+=x;
+//     }
+// }
+// System.out.println("Sum of Even : "+even);
+// System.out.println("Sum of odd : "+odd);
+        int fact =1;
+        for(int i=2;i<=n;i++){
+            fact*=i;
+        }
+        System.out.println("Factorial is : "+fact );
 
     }
 }
