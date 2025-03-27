6f640334e24922d6cabfcd9e5e7adee735006be0
gangadhar195 <gangadhars9545@gmail.com>
2025-03-27 23:27:06 +0530

    Sum of Even Odd no

--- Core Java/SimOfOddEven.java
+++ Core Java/SimOfOddEven.java
@@ -19,11 +19,28 @@ public class SimOfOddEven {
 // }
 // System.out.println("Sum of Even : "+even);
 // System.out.println("Sum of odd : "+odd);
-        int fact =1;
-        for(int i=2;i<=n;i++){
-            fact*=i;
-        }
-        System.out.println("Factorial is : "+fact );
 
+//Questions 2
+        // int fact =1;
+        // for(int i=2;i<=n;i++){
+        //     fact*=i;
+        // }
+        // System.out.println("Factorial is : "+fact );
+
+        //Questions 3 
+int p=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                p++;
+            }
+        
+        }
+        if(p==2){
+            System.out.println("Number is Prime");
+        }
+        else{
+            System.out.println("Number is Not Prime");
+
+        }
     }
 }
