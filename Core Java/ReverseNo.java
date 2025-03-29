69fec224197e3b006a97b95d6acc8f3252682462
gangadhar195 <gangadhars9545@gmail.com>
2025-03-29 14:55:18 +0530

    reverse no

--- Core Java/ReverseNo.java
+++ Core Java/ReverseNo.java
@@ -2,9 +2,17 @@ public class ReverseNo {
     public static void main(String args[]){
         int n=123456;
         int lastno=0;
-        while(n>0){
+        int rev=0;
+        // while(n>0){
+        //     lastno = n%10;
+        //     System.out.print(lastno);
+        //     n=n/10;
+        // }
+
+        while (n>0) {
             lastno = n%10;
-            System.out.print(lastno);
+            rev = (rev*10)+lastno;
+            System.out.println(rev);
             n=n/10;
         }
     }
