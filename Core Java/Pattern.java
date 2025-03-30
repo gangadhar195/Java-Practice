6ffb9d6f4958c0d4c04737c49baab0f0c841eaff
gangadhar195 <gangadhars9545@gmail.com>
2025-03-30 15:35:40 +0530

    Pattern

--- Core Java/Pattern.java
+++ Core Java/Pattern.java
@@ -18,13 +18,20 @@ public class Pattern {
         // }
 
         //Half Pyramid Pattern
-        int n= 4;
+        int n= 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        char ch ='A';
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
-
     }
 }
