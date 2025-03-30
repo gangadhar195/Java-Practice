bc2dac78283c916c0b8b7d3bfbbdc187f27e9dae
gangadhar195 <gangadhars9545@gmail.com>
2025-03-30 12:24:38 +0530

    Prime No

--- Core Java/PrimeNo.java
+++ Core Java/PrimeNo.java
@@ -5,16 +5,36 @@ public class PrimeNo {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number : ");
         int no=sc.nextInt();
-        int div=2;
-        while(div<no){
-            if(no%div==0){
-                System.out.println("Not A Prime No ");
+        // int div=2;
+        // //prime no 1
+        // while(div<no){
+        //     if(no%div==0){
+        //         System.out.println("Not A Prime No ");
                 
+        //     }else{
+        //     div = div+1;
+        //     }
+        // }
+        //     System.out.println("Prime No ");
+
+        //prime no 2
+        boolean Prime = true;
+        if(no==2){
+            System.out.println("Prime No ");
+
+        }
+        else{
+            for(int i=2;i<Math.sqrt(no);i++){
+                if(no%i==0){
+                    Prime=false;
+                }
+            }
+            if(Prime==true){
+                System.out.println("Prime No");
             }else{
-            div = div+1;
+                System.out.println("Not a Prime No");
             }
         }
-            System.out.println("Prime No ");
         
     }
 }
