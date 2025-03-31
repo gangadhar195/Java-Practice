7bcae63c8a9b993186f4f476f746045e002b110c
gangadhar195 <gangadhars9545@gmail.com>
2025-03-31 11:52:48 +0530

    Pattern

--- Core Java/Practice4.java
+++ Core Java/Practice4.java
@@ -110,20 +110,48 @@ public class Practice4 {
         // }
 
         //Question 8 Hollow rhombus pattern
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=n;j++){
+        //         if(i==1 || i==n || j==1 || j==n){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        //Questions 9 Diamond pattern
         int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<n-i+1;j++){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print(" ");
             }
-            for(int j=1;j<=n;j++){
-                if(i==1 || i==n || j==1 || j==n){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-
-                }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 }
