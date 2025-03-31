a68741c04ffb74a51e79435494857bcb59f5658a
gangadhar195 <gangadhars9545@gmail.com>
2025-03-31 11:59:00 +0530

    Pattern

--- Core Java/Practice4.java
+++ Core Java/Practice4.java
@@ -127,28 +127,40 @@ public class Practice4 {
         // }
 
         //Questions 9 Diamond pattern
+        // int n=5;
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Question 10 Number pyramid pattern
         int n=5;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=n-i+1;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n-i+1;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int j=2;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n-i+1;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int j=2;j<=i;j++){
-                System.out.print("*");
+                System.out.print(i+" ");
             }
             System.out.println();
         }
