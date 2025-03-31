f51f3d1285861527578beeb5456caa387e5f33ba
gangadhar195 <gangadhars9545@gmail.com>
2025-03-31 11:18:19 +0530

    Pattern

--- Core Java/Practice4.java
+++ Core Java/Practice4.java
@@ -49,18 +49,52 @@ public class Practice4 {
         //     System.out.println();
         // }
         //Questions 5 01 Triangle pattern
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         int sum=i+j;
+        //         if(sum % 2==0){
+        //             System.out.print(1);
+        //         }
+        //         else{
+        //             System.out.print(0);
+        //         }
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        //Question 6 Butterfly pattern
         int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                int sum=i+j;
-                if(sum % 2==0){
-                    System.out.print(1);
-                }
-                else{
-                    System.out.print(0);
-                }
+                System.out.print("*");
             }
-            System.out.println(" ");
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
