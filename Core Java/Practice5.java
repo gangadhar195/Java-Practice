e5d76d86a5f94ae6d0990c674377346cf50d3e3a
gangadhar195 <gangadhars9545@gmail.com>
2025-04-02 13:24:09 +0530

    Practice 5

--- Core Java/Practice5.java
+++ Core Java/Practice5.java
@@ -1,23 +1,42 @@
 public class Practice5 {
-    public static int Palindrome(int n){
-        int rev=0;
-        while(n>0){
-            int lastDigit = n % 10;
-             rev = rev * 10 + lastDigit;
-            n=n/10;
-        }
-        return rev;
-    }
+    // public static int Palindrome(int n){
+    //     int rev=0;
+    //     while(n>0){
+    //         int lastDigit = n % 10;
+    //          rev = rev * 10 + lastDigit;
+    //         n=n/10;
+    //     }
+    //     return rev;
+    // }
 
+    public static int Factorial(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+             fact = fact * i;
+
+        }
+        return fact;
+    }
+public static 
     public static void main(String args[]){
 
-        int n=124;
-      int re= Palindrome(n);
-        if(n==re){
-            System.out.println("Palindrome: "+ re);
-        }
-        else{
-            System.out.println("Not Palindrome : "+n);
-        }
+    //     int n=124;
+    //   int re= Palindrome(n);
+    //     if(n==re){
+    //         System.out.println("Palindrome: "+ re);
+    //     }
+    //     else{
+    //         System.out.println("Not Palindrome : "+n);
+    //     }
+        int n=5;
+        int r=2;
+
+        int n_fact =Factorial(n);
+        int r_fact =Factorial(r);
+        int rcn_fact =Factorial(n-r);
+
+        int biocoff = n_fact/(r_fact*rcn_fact);
+        System.out.println(biocoff);
+
     }
 }
