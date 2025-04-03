f7b22b511067f1e40dd053333e55e5884dbda2ac
gangadhar195 <gangadhars9545@gmail.com>
2025-04-03 10:51:18 +0530

    Pattrens

--- Core Java/Pattrens.java
+++ Core Java/Pattrens.java
@@ -189,16 +189,63 @@ public class Pattrens {
 //         System.out.println();
 //     }
 
-        int row=5;
-        // int n=1;
-        for(int i=1;i<=row;i++){
-            for(int j=1;j<=i;j++){
-                int n =i+j;
-                if(n%2==0){
-                    System.out.print("1");
-                }else{
-                    System.out.print("0");
-                }
+        // int row=5;
+        // // int n=1;
+        // for(int i=1;i<=row;i++){
+        //     for(int j=1;j<=i;j++){
+        //         int n =i+j;
+        //         if(n%2==0){
+        //             System.out.print("1");
+        //         }else{
+        //             System.out.print("0");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        //Butterfly Pattern
+        // int n=4;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=1;j<n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+
+        //     System.out.println();
+        // }
+        // //Invert Butterfly
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=1;j<n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<n-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+
+        //     System.out.println();
+        // }
+
+        //solid rhombus
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
