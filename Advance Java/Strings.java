5b8a38da1a10b3c09e6befa9530ef76faae0ade1
gangadhar195 <gangadhars9545@gmail.com>
2025-04-04 17:34:30 +0530

    Strings

---
+++ Advance Java/Strings.java
@@ -0,0 +1,24 @@
+public class Strings {
+    public static void main(String args[]){
+        long StartTime = System.currentTimeMillis();
+        StringBuffer buffer = new StringBuffer("Hello");
+        buffer.append(" World");
+        // buffer.delete(2, 3);
+        // buffer.reverse();
+        for(int i=0;i<1000;i++){
+            System.out.println(buffer);
+
+        }
+        System.out.println(System.currentTimeMillis()-StartTime);
+
+    StringBuilder builder = new StringBuilder("Hi");
+    builder.append(" Gangadhar");
+    // System.out.println(builder);
+
+    for(int i=0;i<1000;i++){
+        System.out.println(builder);
+
+    }
+    System.out.println(System.currentTimeMillis()-StartTime);
+    }
+}
