e12e259cac8b4d54a8c051108b34be39229fee87
gangadhar195 <gangadhars9545@gmail.com>
2025-04-05 17:02:35 +0530

    Largest Array Element

--- Advance Java/LargestArray.java
+++ Advance Java/LargestArray.java
@@ -2,13 +2,18 @@ public class LargestArray {
 
     public static int findArrayElement(int arr[]){
         int larget =Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
     
         for(int i=0;i<arr.length;i++){
             if(larget<arr[i]){
                 larget=arr[i];
-
+            }
+            if(smallest>arr[i]){
+                smallest=arr[i];
             }
         }
+        System.out.println("Smallest Array Elememt : "+smallest);
+
         return larget;
 
     }
