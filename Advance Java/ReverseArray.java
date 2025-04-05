c05eba5b28cad6d66bf60fd61816a0a7a3c8a53b
gangadhar195 <gangadhars9545@gmail.com>
2025-04-05 18:32:47 +0530

    Reverse Array

---
+++ Advance Java/ReverseArray.java
@@ -0,0 +1,22 @@
+public class ReverseArray {
+    public static void reverse(int arr[]){
+        int first=0;
+        int last=arr.length-1;
+        while(first<last){
+            int tem=arr[last];
+            arr[last]=arr[first];
+            arr[first]=tem;
+            first++;
+            last--;
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        reverse(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    
+    }
+}
