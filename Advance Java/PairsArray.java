f0031b85926576be1f07aaa791926b617f3e6427
gangadhar195 <gangadhars9545@gmail.com>
2025-04-06 23:49:37 +0530

    Sum of Two

--- Advance Java/PairsArray.java
+++ Advance Java/PairsArray.java
@@ -1,13 +1,17 @@
 public class PairsArray {
     public static void PairsArrayFunctions(int arr[]){
+        int tp=0;
+        
         for(int i=0;i<arr.length;i++){
             int curr= arr[i];
             for(int j=i+1;j<arr.length;j++){
 
                 System.out.print("("+curr+","+arr[j]+") ");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs : "+tp);
     }
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5,};
