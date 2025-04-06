5292d50848f563588ba9afccc4d3d99df06db91c
gangadhar195 <gangadhars9545@gmail.com>
2025-04-06 23:57:42 +0530

    Sum of Two

--- Advance Java/TwoSum.java
+++ Advance Java/TwoSum.java
@@ -2,7 +2,7 @@ public class TwoSum {
     public static int revesrNo(int n){
         int last=0;
         int  rev=0;
-        while(n>0){
+        while(n!=0){
             last=n %10;
             rev = (rev*10)+last;
             n = n/10;
@@ -10,7 +10,7 @@ public class TwoSum {
         return rev;
     }
     public static void main(String args[]){
-        int n=1234;
+        int n=-1234;
 
        int reverse= revesrNo(n);
        System.out.println("reverse no : "+reverse);
