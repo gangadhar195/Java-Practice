751982dbbf3826a8a3497e82e6a6727964dbd51c
gangadhar195 <gangadhars9545@gmail.com>
2025-04-07 23:50:33 +0530

    Practice Array

---
+++ Advance Java/ArrayPractice.java
@@ -0,0 +1,9 @@
+public class ArrayPractice {
+    public static void main(String args[]){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                
+            }
+        }
+    }
+}
