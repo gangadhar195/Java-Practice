12211d3a5cafd910647de0b474df1ca38ea13f5b
gangadhar195 <gangadhars9545@gmail.com>
2025-04-08 00:04:18 +0530

    Practice Array

--- Advance Java/ArrayPractice.java
+++ Advance Java/ArrayPractice.java
@@ -1,16 +1,17 @@
 public class ArrayPractice {
     public static void main(String args[]){
-        for(int i=0;i<4;i++){
-            for(int j=0;j<=i;j++){
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<i;j++){
                 System.out.print("*");
             }
-            for(int j=4;j<4-i+1;j++){
+            for(int j=1;j<=5-i+1;j++){
                 System.out.print(" ");
             }
-            for(int j=4;j<4-i+1;j++){
+            for(int j=1;j<=5-i+1;j++){
                 System.out.print(" ");
             }
-            for(int j=4;j<4-i+1;j++){
+            for(int j=1;j<i;j++){
                 System.out.print("*");
             }
             
