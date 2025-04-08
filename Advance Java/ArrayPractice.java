e93c0e460d542203c872dc8181e1416b6e7e567e
gangadhar195 <gangadhars9545@gmail.com>
2025-04-08 23:37:34 +0530

    Practice Array

--- Advance Java/ArrayPractice.java
+++ Advance Java/ArrayPractice.java
@@ -1,21 +1,35 @@
 public class ArrayPractice {
-    public static void main(String args[]){
-        int n=5;
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<i;j++){
-                System.out.print("*");
-            }
-            for(int j=1;j<=5-i+1;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=5-i+1;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<i;j++){
-                System.out.print("*");
+    public static void FindElement(int arr[],int n){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==n){
+                System.out.println(n+"Found At index : "+i);
+                break;
             }
             
-            System.out.println();
         }
+        
+            System.out.println("Not found ");
+        
+    }
+    public static void main(String args[]){
+        int n=5;
+        int arr[]={1,2,3,4,5,2};
+        FindElement(arr,n);
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=1;j<=5-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=5-i+1;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<i;j++){
+        //         System.out.print("*");
+        //     }
+            
+        //     System.out.println();
+        // }
     }
 }
