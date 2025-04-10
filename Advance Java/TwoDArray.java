3ff91ba6344d2203643945b4daa0c240ea785bcc
gangadhar195 <gangadhars9545@gmail.com>
2025-04-10 22:38:35 +0530

    Practice Array

---
+++ Advance Java/TwoDArray.java
@@ -0,0 +1,18 @@
+public class TwoDArray{
+    public static void main(String[] args) {
+        int arr[][]={
+            {1,4,9},
+            {11,4,3},
+            {2,2,3}
+        };
+        int sum=0;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[1].length;j++){
+                sum +=arr[i][j];
+            }
+            
+        }
+        System.out.println(sum);
+    }
+}
