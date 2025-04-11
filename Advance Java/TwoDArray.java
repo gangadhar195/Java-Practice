349831e28e4503c86bfcf827699be96a2030b11b
gangadhar195 <gangadhars9545@gmail.com>
2025-04-11 23:49:42 +0530

    New

--- Advance Java/TwoDArray.java
+++ Advance Java/TwoDArray.java
@@ -6,7 +6,9 @@ public class TwoDArray{
             {2,2,3}
         };
         for(int i=0;i<arr.length;i++){
-            
+                for(int j=0;j<arr.length;j++){
+                    System.out.print(arr[i][j]+"\t");
+                }
         }
     }
 }
