c0bac227765ec74a107ffdb1ff870264c9125f9e
gangadhar195 <gangadhars9545@gmail.com>
2025-04-11 23:46:39 +0530

    New

--- Advance Java/TwoDArray.java
+++ Advance Java/TwoDArray.java
@@ -5,15 +5,8 @@ public class TwoDArray{
             {11,4,3},
             {2,2,3}
         };
-        int sum=0;
-        int n=0;
-
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                sum +=arr[i][j];
-            }
             
         }
-        System.out.println(sum);
     }
 }
