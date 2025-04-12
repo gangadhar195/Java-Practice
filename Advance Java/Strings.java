4695b4f1fb772caf6fe20c493d6bab64c965e634
gangadhar195 <gangadhars9545@gmail.com>
2025-04-12 23:55:35 +0530

    String

--- Advance Java/Strings.java
+++ Advance Java/Strings.java
@@ -1,24 +1,27 @@
 public class Strings {
     public static void main(String args[]){
         long StartTime = System.currentTimeMillis();
-        StringBuffer buffer = new StringBuffer("Hello");
-        buffer.append(" World");
-        // buffer.delete(2, 3);
-        // buffer.reverse();
-        for(int i=0;i<1000;i++){
-            System.out.println(buffer);
+    //     StringBuffer buffer = new StringBuffer("Hello");
+    //     buffer.append(" World");
+    //     // buffer.delete(2, 3);
+    //     // buffer.reverse();
+    //     for(int i=0;i<1000;i++){
+    //         System.out.println(buffer);
 
-        }
-        System.out.println(System.currentTimeMillis()-StartTime);
+    //     }
+    //     System.out.println(System.currentTimeMillis()-StartTime);
 
-    StringBuilder builder = new StringBuilder("Hi");
-    builder.append(" Gangadhar");
-    // System.out.println(builder);
+    // StringBuilder builder = new StringBuilder("Hi");
+    // builder.append(" Gangadhar");
+    // // System.out.println(builder);
 
-    for(int i=0;i<1000;i++){
-        System.out.println(builder);
+    // for(int i=0;i<1000;i++){
+    //     System.out.println(builder);
 
-    }
-    System.out.println(System.currentTimeMillis()-StartTime);
+    // }
+    // System.out.println(System.currentTimeMillis()-StartTime);
+
+    String str1="Hello";
+    String str2="Word";
     }
 }
