67cfef2c2c18f5527fae2ff583ac5ddee8ecded0
gangadhar195 <gangadhars9545@gmail.com>
2025-04-16 19:45:29 +0530

    Vector Example

---
+++ Advance Java/Collections/VectorExample.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String args[]){
+        Vector<String> vet= new Vector<String>();
+        vet.add("John");
+        vet.add("Roy");
+        vet.add("Filip Jey");
+        System.out.println(vet.size());
+        System.out.println(vet);
+        vet.remove(2);
+        for( String value:vet){
+            System.out.println(value);
+        }
+        System.out.println(vet.size());
+
+    }
+}
