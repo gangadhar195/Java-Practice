6e5099c1b7ab2998e56f830e93d67fba061d7118
gangadhar195 <gangadhars9545@gmail.com>
2025-04-17 16:17:03 +0530

    Queue Example

--- Advance Java/Collections/QueueExample.java
+++ Advance Java/Collections/QueueExample.java
@@ -3,17 +3,30 @@ import java.util.*;
 
 public class QueueExample {
     public static void main(String args[]){
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-    System.out.println(queue.size());
-    queue.add(10);
-    queue.add(20);
-    queue.add(30);
-    queue.add(40);
-    System.out.println(queue.size());
-    System.out.println(queue.peek());
-    System.out.println(queue.poll());
-    System.out.println(queue.size());
+    //     PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+    // System.out.println(queue.size());
+    // queue.add(10);
+    // queue.add(20);
+    // queue.add(30);
+    // queue.add(40);
+    // System.out.println(queue.size());
+    // System.out.println(queue.peek());
+    // System.out.println(queue.poll());
+    // System.out.println(queue.size());
+    // System.out.println(queue.isEmpty());
 
+    Queue<Integer> que = new LinkedList<>();
+    System.out.println(que.size());
+    que.add(10);
+    que.add(20);
+    que.add(30);
+    que.add(40);
+    System.out.println(que);
+    System.out.println(que.size());
+    System.out.println(que.peek());
+    System.out.println(que.poll());
+    System.out.println(que);
+    
 
     }
 }
