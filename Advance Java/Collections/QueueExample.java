2d1252b8a674fb882f9481de0e0d342d4bc03fba
gangadhar195 <gangadhars9545@gmail.com>
2025-04-17 15:20:27 +0530

    Queue Example

---
+++ Advance Java/Collections/QueueExample.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class QueueExample {
+    public static void main(String args[]){
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+    System.out.println(queue.size());
+    queue.add(10);
+    queue.add(20);
+    queue.add(30);
+    queue.add(40);
+    System.out.println(queue.size());
+    System.out.println(queue.peek());
+    System.out.println(queue.poll());
+    System.out.println(queue.size());
+
+
+    }
+}
