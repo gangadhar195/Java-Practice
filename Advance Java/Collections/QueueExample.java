b954baad6d7d2acee37d4747d7d3586953d6d920
gangadhar195 <gangadhars9545@gmail.com>
2025-04-17 23:26:17 +0530

    Queue Example

--- Advance Java/Collections/QueueExample.java
+++ Advance Java/Collections/QueueExample.java
@@ -15,18 +15,24 @@ public class QueueExample {
     // System.out.println(queue.size());
     // System.out.println(queue.isEmpty());
 
-    Queue<Integer> que = new LinkedList<>();
-    System.out.println(que.size());
-    que.add(10);
-    que.add(20);
-    que.add(30);
-    que.add(40);
-    System.out.println(que);
-    System.out.println(que.size());
-    System.out.println(que.peek());
-    System.out.println(que.poll());
-    System.out.println(que);
+    // Queue<Integer> que = new LinkedList<>();
+    // System.out.println(que.size());
+    // que.add(10);
+    // que.add(20);
+    // que.add(30);
+    // que.add(40);
+    // System.out.println(que);
+    // System.out.println(que.size());
+    // System.out.println(que.peek());
+    // System.out.println(que.poll());
+    // System.out.println(que);
     
-
+    // add item using for loop
+    Queue<Integer> que = new LinkedList<>();
+    for(int i=1;i<=10;i++){
+        que.add(i);
+    }
+    System.out.println(que.size());
+    System.out.println(que);
     }
 }
