b5e741a5531da1190638df574a9be4182e0fdbb1
gangadhar195 <gangadhars9545@gmail.com>
2025-04-20 23:58:29 +0530

    Water tank Example

--- Advance Java/WaterCapacity.java
+++ Advance Java/WaterCapacity.java
@@ -1,7 +1,13 @@
 public class WaterCapacity {
     public static void main(String args[]){
         System.out.println("Hello word");
-        int arr[][] = new int[][];
-        
+        // int arr[][] = new int[][];
+        int n=8;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                
+            }
+        }
+
     }
 }
