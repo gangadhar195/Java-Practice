1a2e9bcc2fee554ee1c6f2df2cbb42493d968b29
gangadhar195 <gangadhars9545@gmail.com>
2025-04-22 23:43:18 +0530

    Merge Sort example

--- Advance Java/MergeSort.java
+++ Advance Java/MergeSort.java
@@ -1,8 +1,14 @@
 public class MergeSort {
+
+    public static int Merge(int a1[],int a2[]){
+        
+        return 0;
+    }
     public static void main(String args[]){
         int a1[] = {10,20,30,40,50};
         int a2[] = { 20,70,80,90};
 
-        int a3 = merge(a1,a2);
+        int a3 = Merge(a1,a2);
+
     }
 }
