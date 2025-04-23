ce7782ebd1e07a7660e770c48743966ed9b2832f
gangadhar195 <gangadhars9545@gmail.com>
2025-04-23 23:24:01 +0530

    Merge Sort Example

--- Advance Java/MergeSort.java
+++ Advance Java/MergeSort.java
@@ -10,11 +10,11 @@ public class MergeSort {
                     int val2 =(p2<n2)?a2[p2]:Integer.MAX_VALUE;
 
                     if(val1<=val2){
-                        a3[p1]=val1;
+                        a3[i]=val1;
                         p1++;
                     }
                     else{
-                        a3[p2]=val2;
+                        a3[i]=val2;
                         p2++;
                     }
                 }
@@ -22,9 +22,10 @@ public class MergeSort {
     }
     public static void main(String args[]){
         int a1[] = {10,20,30,40,50};
-        int a2[] = { 20,70,80,90};
+        int a2[] = {20,70,80,90,20};
 
         int a3[] = Merge(a1,a2);
+    
         for(int i=0;i<a3.length;i++){
             System.out.print(a3[i]+"\t");
         }
