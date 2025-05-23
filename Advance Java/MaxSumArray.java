e27e903b2efefb86a65b1edf7e30031df14e285a
gangadhar195 <gangadhars9545@gmail.com>
2025-05-23 15:44:06 +0530

    Sum of Max Array

--- Advance Java/MaxSumArray.java
+++ Advance Java/MaxSumArray.java
@@ -3,15 +3,23 @@ public class MaxSumArray {
    public static void PrintMaxArraySum(int numbers[]){
             int currSum = 0;
             int MaxVal = Integer.MIN_VALUE;
+            int prefix[] = new int[numbers.length];
+            prefix[0]= numbers[0];
+            
+            for(int i=1;i<prefix.length;i++){
+                prefix[i] = prefix[i-1] + numbers[i];
+            }
             for(int i=0;i<numbers.length;i++){
                int start = i;
                for(int j=i;j<numbers.length;j++){
                 int end = j;
                 currSum=0;
-                for(int k=start;k<=end;k++){
-                    currSum += numbers[k];
-                }
-                System.out.println(currSum);
+
+                currSum = start == 0 ? prefix[end]:prefix[end]-prefix[start-1];
+                // for(int k=start;k<=end;k++){
+                //     currSum += numbers[k];
+                // }
+                // System.out.println(currSum);
                 if(MaxVal < currSum){
                     MaxVal = currSum;
                 }
