149479bc6645a05a8c3354c94bdc384aed4b14dd
gangadhar195 <gangadhars9545@gmail.com>
2025-05-25 17:54:30 +0530

    Insertion Sort

---
+++ Sorting/CountingSort.java
@@ -0,0 +1,16 @@
+public class CountingSort{
+
+    public static void countingsort(int arr[]){
+
+    }
+    public static void Printarry(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,1,6,3,11,11,43,23};
+        countingsort(arr);
+        Printarry(arr);
+    }
+}
