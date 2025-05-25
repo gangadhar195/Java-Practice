7e5cb1f49f517745586d2e8e10aca0bff57e682b
gangadhar195 <gangadhars9545@gmail.com>
2025-05-25 14:01:09 +0530

    bubble sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+public class BubbleSort {
+    public static void bubblesort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void Printarry(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,1,6,3};
+        bubblesort(arr);
+        Printarry(arr);
+    }
+}
