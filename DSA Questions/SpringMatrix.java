0c205ef86185de7bcb68f22f97e85d7b7013f20a
gangadhar195 <gangadhars9545@gmail.com>
2025-05-26 19:00:15 +0530

    Spring Matrix Example

---
+++ DSA Questions/SpringMatrix.java
@@ -0,0 +1,47 @@
+public class SpringMatrix {
+    public static void PrintSpringMatrixValue(int matrix[][]) {
+        int startRow = 0;
+        int endCol = matrix.length - 1;
+        int endRow = matrix[0].length - 1;
+        int startCol = 0;
+        while (startRow <= endRow && startCol <= endCol) {
+            //topcol
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j]+" ");
+            }
+            //rightcol
+            for(int i=startRow+1;i<=endRow;i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //bottom
+            for(int j=endCol-1;j>=startCol;j--){
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            //startcol
+            for(int i=endRow-1;i>=startRow+1;i--){
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startCol++;
+            startRow++;
+            endRow--;
+            endCol--;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+        // for(int i=0;i<matrix.length;i++){
+        // for(int j=0;j<matrix[0].length;j++){
+        // System.out.print(matrix[i][j]+" ");
+        // }
+        // System.out.println();
+        // }
+        PrintSpringMatrixValue(matrix);
+    }
+}
