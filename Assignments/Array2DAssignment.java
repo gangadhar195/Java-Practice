5c1172492c532ed7d103a619759ce5d7d5f68142
gangadhar195 <gangadhars9545@gmail.com>
2025-05-27 12:27:11 +0530

    Assignment questions

--- Assignments/Array2DAssignment.java
+++ Assignments/Array2DAssignment.java
@@ -1,23 +1,37 @@
 package Assignments;
 
 public class Array2DAssignment {
-    public static void FindArrayElement(int array[][],int key){
-        int sum=0;
-        for(int i=0;i<array.length;i++){
-            for(int j=0;j<array[0].length;j++){
-                if(key==array[i][j]){
-                sum++;
-            }
-            }
+    // public static void FindArrayElement(int array[][],int key){
+    //     int sum=0;
+    //     for(int i=0;i<array.length;i++){
+    //         for(int j=0;j<array[0].length;j++){
+    //             if(key==array[i][j]){
+    //             sum++;
+    //         }
+    //         }
             
+    //     }
+    //     System.out.println(sum);
+    // }
+    public static void SumOfSecondRow(int array[][]){
+        int sum=0;
+        int row=1,col=array.length;
+        for(int i=0;i<col;i++){
+            sum+=array[row][i];
         }
-        System.out.println(sum);
+        System.out.println("Sum of Second Row is : "+sum);
     }
     public static void main(String args[]){
+        // int array[][] = {
+        //     {4,7,8,6},
+        //     {8,8,7,7}
+        // };
+        // FindArrayElement(array,8);
         int array[][] = {
-            {4,7,8,6},
-            {8,8,7,7}
+            {1,4,9},
+            {11,4,3},
+            {2,2,3}
         };
-        FindArrayElement(array,8);
+        SumOfSecondRow(array);
     }
 }
