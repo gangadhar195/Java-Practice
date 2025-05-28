bc6fb141d1b30f09088d349bff78fb1ec477e6e9
gangadhar195 <gangadhars9545@gmail.com>
2025-05-28 23:27:16 +0530

    Palindrome Program

--- String/FindPalindrome.java
+++ String/FindPalindrome.java
@@ -1,8 +1,18 @@
 package String;
 
 public class FindPalindrome {
+        public static boolean isPalindrome(String Num){
+            int n = Num.length();
+            for(int i=0;i<Num.length()/2;i++){
+                if(Num.charAt(i)== Num.charAt(n-1-i)){
+                    return false;
+                }
+            }
+            return true;
+        }
     public static void main(String args[]){
         String Num = "1234321";
-        
+
+        System.out.println(isPalindrome(Num));
     }
 }
