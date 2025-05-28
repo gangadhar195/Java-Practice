29ead447f58602565d8d7f979fd88ce94541bff2
gangadhar195 <gangadhars9545@gmail.com>
2025-05-29 00:02:37 +0530

    Shortest Path Program

---
+++ DSA Questions/ShortPath.java
@@ -0,0 +1,28 @@
+public class ShortPath {
+    public static float FindShortestPath(String str){
+        int x=0,y=0;
+        for(int i=0;i<str.length();i++){
+            char dir = str.charAt(i);
+            if(dir=="N"){
+                y++;
+            }
+            else if(dir=="W"){
+                x--;
+            }
+            else if(dir=="S"){
+                y--;
+            }
+            else{
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+    }
+    public static void main(String args[]){
+        String str = "NSEWWESMM";
+        // FindShortestPath(str);
+        System.out.println(FindShortestPath(str));
+    }
+}
