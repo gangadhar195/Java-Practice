5d183a2a0208df7e1051d3f3b4fb0a99b913a24c
gangadhar195 <gangadhars9545@gmail.com>
2025-05-30 15:09:38 +0530

    String Builder

--- String/StringBuilderMethod.java
+++ String/StringBuilderMethod.java
@@ -4,7 +4,7 @@ public class StringBuilderMethod {
     public static void main(String args[]){
         StringBuilder str = new StringBuilder("");
         for(char i='a';i<='z';i++){
-            str.append(" "+i);
+            str.append(i);
         }
         System.out.println(str);
         System.out.println(str.length());
