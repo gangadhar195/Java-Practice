e9f3f4758a5c42e958abeec8028bf55ff05899dc
gangadhar195 <gangadhars9545@gmail.com>
2025-05-31 19:42:11 +0530

    OOPS Concept

---
+++ OOPS/ConstructorExam.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class ConstructorExam {
+    public static void main(String args[]){
+        Student s1=new Student();
+    }
+}
+class Student{
+    String name;
+    int roll;
+    Student(){
+        System.out.println("Constructor is called..");
+    }
+}
