f345c0f2b8fb254838c03371ac82c4373e1decbc
gangadhar195 <gangadhars9545@gmail.com>
2025-05-31 18:48:26 +0530

    OOPS Concept

--- OOPS/BasicClassAndObj.java
+++ OOPS/BasicClassAndObj.java
@@ -4,19 +4,25 @@ public class BasicClassAndObj {
     public static void main(String args[]){
     Pen p1 = new Pen();
     p1.setColor("Red");
-    System.out.println(p1.color);
-    p1.color="Green";
-    System.out.println(p1.color);
+    System.out.println(p1.getColor());
+    // p1.color="Green";
+    System.out.println(p1.getColor());
     p1.setTip(10);
-    p1.tip=20;
-    System.out.println(p1.tip);
+    // p1.tip=20;
+    System.out.println(p1.getTip());
     
  }
 }
 
 class Pen{
-   public String color;
+   private String color;
    protected int tip;
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
     void setColor(String newColor){
         color=newColor;
     }
