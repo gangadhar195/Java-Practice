cf96ed5da0928572c4897ff04e677c2b8852afb7
gangadhar195 <gangadhars9545@gmail.com>
2025-06-01 07:59:10 +0530

    Inheritance

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -2,26 +2,66 @@ package OOPS;
 
 public class Inheritance {
     public static void main(String args[]){
-        Fish shark = new Fish();
-        shark.Name();
-        shark.Swwing();
-        shark.Eats();
+        //sigle inheritance
+        // Fish shark = new Fish();
+        // shark.Name();
+        // shark.Swwing();
+        // shark.Eats();
+
+        //multilevel inheritance
+        // Paplate p = new Paplate();
+        // p.Name();
+        // p.Swwing();
+        // p.papy();
+
+        //Hirarchical inheritance
+        Lion l=new Lion();
+        l.Eats();
+        Fish f = new Fish();
+        f.Eats();
+
     }
 }
 
+// class Animal{
+//     String name;
+//     String eats;
+//     void Eats(){
+//         System.out.println("Eats");
+//     }
+//     void Name(){
+//         System.out.println("Name");
+//     }
+// }
+// class Fish extends Animal{
+//     //child
+//     String swwing;
+//     void Swwing(){
+//         System.out.println("Fish Swwing in Water");
+//     }
+// }
+// //grand child
+// class Paplate extends Fish{
+//     String size="Faty";
+//     void papy(){
+//         System.out.println(size +" ");
+//     }
+// }
+
+//Base
 class Animal{
-    String name;
-    String eats;
-    void Eats(){
-        System.out.println("Eats");
-    }
-    void Name(){
-        System.out.println("Name");
-    }
+    String eat;
+   void Eats(){
+    System.out.println("Eats Non-veage");
+   }
 }
 class Fish extends Animal{
-    String swwing;
-    void Swwing(){
-        System.out.println("Fish Swwing in Water");
+void EatsFish(){
+        System.out.println("Live in Water");
     }
 }
+ class Lion extends Animal{
+ void EatsLion(){
+        System.out.println("live in Land");
+    }
+ }
