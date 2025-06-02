3ffe8ca42d29a6adcff8d1c7daffccacce0742b2
gangadhar195 <gangadhars9545@gmail.com>
2025-06-02 20:47:14 +0530

    Abstract Class

--- OOPS/AbstractClass.java
+++ OOPS/AbstractClass.java
@@ -4,20 +4,29 @@ public class AbstractClass {
     public static void main(String args[]){
         Fish f= new Fish();
         f.Eates();
-        Horse h = new Horse();
-        h.Eats();
+        // Horse h = new Horse();
+        // h.Eats();
     }
 }
 
 abstract class Animal{
+    Animal(){
+        System.out.println("Animal class");
+    }
     abstract void Eats();
 }
 class Horse extends Animal{
+    Horse(){
+        System.out.println("Horse Class");
+    }
     void Eats(){
         System.out.println("Eates grass");
     }
 }
 class Fish extends Horse{
+    Fish(){
+        System.out.println("Fish class");
+    }
     void Eates(){
         System.out.println("Eats small fish");
     }
