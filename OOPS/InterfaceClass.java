aa62f04feebfed285adc0e04e5d7128cdb1f2b4b
gangadhar195 <gangadhars9545@gmail.com>
2025-06-02 22:09:47 +0530

    Interface Class Example

---
+++ OOPS/InterfaceClass.java
@@ -0,0 +1,28 @@
+package OOPS;
+
+public class InterfaceClass {
+    public static void main(String args[]){
+
+    }
+}
+  interface ChessPlayer {
+    void move();
+ }
+
+ class Queen implements ChessPlayer{
+   public void move(){
+            System.out.println("up,down,left,right,diagonal(in 4 direction)");
+    }
+ }
+ class Rooke implements ChessPlayer{
+    public void move(){
+            System.out.println("up,down,left,right");
+
+    }
+ }
+ class King implements ChessPlayer{
+    public void move(){
+            System.out.println("up,down,left,right,diagonal(by 1 step)");
+
+    }
+ }
