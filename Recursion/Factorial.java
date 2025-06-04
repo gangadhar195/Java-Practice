ba9955e776e3fea7a099453e319e1b22eed978e8
gangadhar195 <gangadhars9545@gmail.com>
2025-06-04 19:51:37 +0530

    Factorial Program

---
+++ Recursion/Factorial.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Factorial {
+    public static int Fact(int n){
+        // if(n==0){
+        //     return 1;
+        // }else{
+        //     return n * Fact(n-1);
+        // }
+
+        if(n>0){
+            return n*Fact(n-1);
+        }else{
+            return 1;
+        }
+    }
+    public static void main(String args[]){
+        int result = Fact(5);
+        System.out.println("Factorial is : "+result);
+    } 
+}
