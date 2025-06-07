be44c2242ddce7882f25e6f81c9f76535e66d9be
gangadhar195 <gangadhars9545@gmail.com>
2025-06-07 19:28:31 +0530

    first occurence

---
+++ Recursion/CheckArray.java
@@ -0,0 +1,29 @@
+public class CheckArray{
+
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+
+    public static int firstOccurence(int arr[],int key,int i){
+        if(i==arr.length){
+            return -1;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+       return firstOccurence(arr, key, i+1);
+    }
+    public static void main(String args[]){
+        int arr[] ={1,2,3,6,4,5,6};
+        // System.out.println(isSorted(arr, 0));
+        // firstOccurence(arr, 6, 0);
+        System.out.println(firstOccurence(arr, 6, 0));
+
+    }
+}
