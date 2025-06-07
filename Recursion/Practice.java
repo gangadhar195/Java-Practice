2c46a5d589d03753e9fcd936f20759c2301c9e0f
gangadhar195 <gangadhars9545@gmail.com>
2025-06-07 14:08:25 +0530

    Recursion Increase and decrease order

--- Recursion/Practice.java
+++ Recursion/Practice.java
@@ -1,15 +1,26 @@
 public class Practice {
     public static void DecreasingOrder(int n){
         if(n==1){
-            System.out.print(n);
+            System.out.print(n+" ");
             return ;
         }
             System.out.print(n+" ");
          DecreasingOrder(n-1);
     }
+    
+    public static void IncrePrint(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        IncrePrint(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String args[]){
         int n =10;
         DecreasingOrder(n);
+         System.out.println();
+        IncrePrint(n);
         // System.out.println(DecreasingOrder(n));
     }
 }
