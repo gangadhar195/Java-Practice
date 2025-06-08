b12337bc18db358d6a8e7522c1141eccbbeef189
gangadhar195 <gangadhars9545@gmail.com>
2025-06-08 10:41:59 +0530

    first and last occurence

--- Recursion/CheckArray.java
+++ Recursion/CheckArray.java
@@ -19,11 +19,22 @@ public class CheckArray{
         }
        return firstOccurence(arr, key, i+1);
     }
+    public static int lastOccurence(int arr[],int key,int i){
+        if(i==arr.length){
+            return -1;
+        }
+        int isFound = lastOccurence(arr, key, i+1);
+        if(isFound ==-1 && arr[i]==key){
+            return i;
+        }
+        return isFound;
+    }
     public static void main(String args[]){
         int arr[] ={1,2,3,6,4,5,6};
         // System.out.println(isSorted(arr, 0));
         // firstOccurence(arr, 6, 0);
-        System.out.println(firstOccurence(arr, 6, 0));
+        // System.out.println(firstOccurence(arr, 6, 0));
+        System.out.println(lastOccurence(arr,6,0));
 
     }
 }
