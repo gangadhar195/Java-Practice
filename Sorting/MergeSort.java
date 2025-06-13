51f4ce5a32f229e84bdbab5d048cfaf84937eb7e
gangadhar195 <gangadhars9545@gmail.com>
2025-06-13 10:39:41 +0530

    Merge Sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -10,11 +10,11 @@ public class MergeSort {
     }
     public static void merge(int arr[],int mid,int si,int ei){
         int temp[]= new int[ei-si+1];
-        int i=arr[si];
-        int j=arr[mid+1];
+        int i=si;
+        int j=mid+1;
         int k=0;
         while(i<=mid && j<=ei){
-            if(arr[i]<arr[j]){
+            if(arr[i]<=arr[j]){
                 temp[k]=arr[i];
                 i++;
             }
@@ -41,7 +41,7 @@ public class MergeSort {
         System.out.println();
     }
     public static void main(String args[]){
-        int arr[] = {2,5,1,6,4,8};
+        int arr[] = {2,5,1,6,4,8,3,2,3};
         // int si=arr[0];
         // int ei = arr.length;
         Printmergesort(arr,0,arr.length-1);
