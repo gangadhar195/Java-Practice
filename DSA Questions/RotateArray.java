1c5e5fdfa8b7c04f8d7c40cb38ab874e99cf85fb
gangadhar195 <gangadhars9545@gmail.com>
2025-06-15 17:31:48 +0530

    Rotate Array Problem

---
+++ DSA Questions/RotateArray.java
@@ -0,0 +1,44 @@
+public class RotateArray {
+
+    public static int search(int arr[],int tar,int si,int ei){
+        if(si>ei){
+            return -1;
+        }
+        int mid = si+(ei-si)/2;
+
+        if(arr[mid]==tar){
+            return mid;
+        }
+
+        //Line one
+        if(arr[si]<=arr[mid]){
+            //left side
+            if(arr[si]<=tar && tar <= arr[mid]){
+                return search(arr, tar, si, mid-1);
+            }
+            //right side
+            else{
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+
+        //Line two 
+        else{
+            //left side
+            if(arr[mid]<=tar && tar <= arr[ei]){
+                return search(arr, tar, mid+1, ei);
+            }else{
+                //right side
+                return search(arr, tar, si, mid-1);
+            }
+        }
+    }
+
+    public static void main(String args[]){
+    int arr[] = {2,4,6,8,9,1,5};
+    int tar = 1;
+   int tarIdx= search(arr,tar,0,arr.length-1);
+   System.out.println(tarIdx);
+
+    }
+}
