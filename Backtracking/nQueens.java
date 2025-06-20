a30bb91259e324e6861cd21e248b7b2241c98904
gangadhar195 <gangadhars9545@gmail.com>
2025-06-20 16:39:02 +0530

    NQueens

--- Backtracking/nQueens.java
+++ Backtracking/nQueens.java
@@ -1,14 +1,38 @@
 public class nQueens {
 
+    public static boolean isSafe(char board[][],int row,int col){
+        //vertical
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+
+        //daigonal left up
+        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //daigonal right up
+        for(int i=row-1,j=col+1;i>=0 && j<=board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void nqueens(char board[][],int row){
         if(row==board.length){
             printCahr(board);
             return;
         }
         for(int i=0;i<board.length;i++){
+            if(isSafe(board,row,i)){
             board[row][i]='Q';
             nqueens(board, row+1);
             board[row][i]='X';
+            }
         }
     }
 
