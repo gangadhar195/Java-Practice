bd71b8682313b758f7b7d74ab77721f39ba764fc
gangadhar195 <gangadhars9545@gmail.com>
2025-06-24 23:55:47 +0530

    String Reverse

--- Assignments/Practics.java
+++ Assignments/Practics.java
@@ -1,14 +1,26 @@
 package Assignments;
 
 public class Practics {
-    public static void main(String args[]){
-        String str = "Java";
-        String rev = "";
 
+    public static void reverseString(String str){
+        StringBuilder newstr = new StringBuilder();
         System.out.println("old String : "+str);
         for(int i=str.length()-1;i>=0;i--){
-            rev += str.charAt(i);
+            newstr.append(str.charAt(i));
         }
-        System.out.println("Reverse String : "+rev);
+        System.out.println("Reverse String : "+newstr);
+
+    }
+    public static void main(String args[]){
+        String str = "gangadhar";
+        // String rev = "";
+
+        // System.out.println("old String : "+str);
+        // for(int i=str.length()-1;i>=0;i--){
+        //     rev += str.charAt(i);
+        // }
+        // System.out.println("Reverse String : "+rev);
+
+        reverseString(str);
     }
 }
