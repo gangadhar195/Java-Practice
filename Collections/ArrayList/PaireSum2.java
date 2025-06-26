12b836bece923b1bcecf0a816e4357f8937271a7
gangadhar195 <gangadhars9545@gmail.com>
2025-06-26 18:27:24 +0530

    Paire of 2 target

---
+++ Collections/ArrayList/PaireSum2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class PaireSum2 {
+
+    public static void sumoftwo(ArrayList<Integer> list,int target){
+        int sum=0;
+        int j;
+        for( int i=0;i<list.size();i++){
+            for( j=1;j<list.size();j++){
+                sum = list.get(i)+list.get(j);
+                 if(sum==target){
+                System.out.println(list.get(i)+" "+ list.get(j));
+                return;
+            }
+            }
+           
+            
+        }
+    }
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int target = 16;
+        sumoftwo(list,target);
+
+
+    }
+}
