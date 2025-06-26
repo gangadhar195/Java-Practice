6a4bb35ac32cf81e5d978ad0a9da0bead0b181d9
gangadhar195 <gangadhars9545@gmail.com>
2025-06-26 18:35:24 +0530

    Collections Sort

---
+++ Collections/ArrayList/SortingArraylist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortingArraylist {
+   public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+
+   }
+ 
+}
