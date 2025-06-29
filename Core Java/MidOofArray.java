9ef7030634bda10c8b92aec71d4f6e9b348134cc
gangadhar195 <gangadhars9545@gmail.com>
2025-06-29 11:07:51 +0530

    find mid of array

--- Core Java/MidOofArray.java
+++ Core Java/MidOofArray.java
@@ -1,37 +1,45 @@
+import java.util.Arrays;
+
 public class MidOofArray {
 
-     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int mergeArray[]= new int[nums1.length+nums2.length];
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int mergeArray[] = new int[nums1.length + nums2.length];
         double mid = 0;
-        for(int i=0;i<nums1.length;i++){
-            mergeArray[i]=nums1[i];
+
+        // Merge both arrays
+        for (int i = 0; i < nums1.length; i++) {
+            mergeArray[i] = nums1[i];
         }
-        for(int i=0;i<nums2.length;i++){
-            mergeArray[nums1.length+i]=nums2[i];
+        for (int i = 0; i < nums2.length; i++) {
+            mergeArray[nums1.length + i] = nums2[i];
         }
+
+        // Sort the merged array
+        Arrays.sort(mergeArray);
+
         int n = mergeArray.length;
 
-         if (n == 0) {
+        if (n == 0) {
             System.out.println("Array is empty.");
             return 0;
         }
 
+        // Median calculation
         if (n % 2 == 0) {
-            
-            mid =  (mergeArray[(n / 2) - 1] + mergeArray[n / 2])/2;
-            return mid;
+            mid = (mergeArray[(n / 2) - 1] + mergeArray[n / 2]) / 2.0; // Note: 2.0 for double division
         } else {
-            
-            mid =  mergeArray[n / 2];
-            return mid;
+            mid = mergeArray[n / 2];
         }
-        
-    }
-    public static void main(String args[]){
-        int num1[] = {1,3};
-        int num2[] = {2};
-        // findMedianSortedArrays(nums1,nums2);
 
-        System.out.println(findMedianSortedArrays(num1,num2));
+        return mid;
+    }
+
+    public static void main(String args[]) {
+        int num1[] = {1, 2};
+        int num2[] = {3,4};
+
+        MidOofArray obj = new MidOofArray(); // Create object
+        double result = obj.findMedianSortedArrays(num1, num2);
+        System.out.println(result);
     }
 }
