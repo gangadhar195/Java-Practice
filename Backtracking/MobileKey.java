47eea2d08ee116c2e04a0bfb0b78b9b9a072b3a6
gangadhar195 <gangadhars9545@gmail.com>
2025-07-01 13:21:36 +0530

    Link-List firstadd and lastadd

---
+++ Backtracking/MobileKey.java
@@ -0,0 +1,15 @@
+public class MobileKey {
+
+    public static void mobilekey(String[] key,int b1,int b2){
+        for(int i=0;i<key.length;i++){
+            
+        }
+    }
+    public static void main(String args[]){
+        String key[] = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        // int digit[] = {2,3,4,5,6,7,8,9};
+        int b1=2,b2=3;
+
+        mobilekey(key,b1,b2);
+    }
+}
