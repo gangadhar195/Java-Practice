423340de7127abd1f0c4c3bbb1458c4a505e1917
gangadhar195 <gangadhars9545@gmail.com>
2025-07-02 12:07:50 +0530

    LinkedList add index

---
+++ LinkedList/LinkedOne.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+public class LinkedOne {
+    public static class Node{
+        int data;
+        Node next;
+
+        public  Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+
+        head=newNode;
+    }
+
+    public static void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public static void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        LinkedOne ll = new LinkedOne();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+
+        
+    }
+}
