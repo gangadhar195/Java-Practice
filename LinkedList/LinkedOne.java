1b33d03a5c4a220cd3e468dad808c615a840b922
gangadhar195 <gangadhars9545@gmail.com>
2025-07-02 13:21:06 +0530

    LinkedList add index and remove first idx

--- LinkedList/LinkedOne.java
+++ LinkedList/LinkedOne.java
@@ -13,9 +13,11 @@ public class LinkedOne {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public static void addFirst(int data){
         Node newNode = new Node(data);
+        size++;
         if(head==null){
             head=tail=newNode;
             return;
@@ -27,6 +29,7 @@ public class LinkedOne {
 
     public static void addLast(int data){
         Node newNode=new Node(data);
+        size++;
         if(head==null){
             head=tail=newNode;
             return;
@@ -35,12 +38,13 @@ public class LinkedOne {
         tail=newNode;
     }
     public void add(int idx,int data){
-        Node newNode = new Node(data);
 
         if(idx==0){
             addFirst(data);
             return;
         }
+        Node newNode = new Node(data);
+        size++;
         Node temp=head;
         int i=0;
         while(i<idx-1){
@@ -51,6 +55,20 @@ public class LinkedOne {
         temp.next=newNode;
     }
 
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MAX_VALUE;
+        }else if(size==1){
+            int val =head.data;
+            head=tail=null;
+            return val;
+        }
+        int val =head.data;
+        head=head.next;
+        return val;
+    }
+
     public static void print(){
         Node temp=head;
         while(temp!=null){
@@ -58,19 +76,22 @@ public class LinkedOne {
             temp=temp.next;
         }
         System.out.println("null");
+        // System.out.println(size);
     }
     public static void main(String args[]){
         LinkedOne ll = new LinkedOne();
-        ll.addFirst(1);
-        // ll.print();
         ll.addFirst(2);
         // ll.print();
-        ll.addLast(3);
-        // ll.print();
-        ll.addLast(4);
-        ll.print();
-        // ll.print();
-       ll.add(2, 11);
+    //     ll.addFirst(1);
+    //     // ll.print();
+    //     ll.addLast(3);
+    //     // ll.print();
+    //     ll.addLast(4);
+    //     ll.print();
+    //     // ll.print();
+    //    ll.add(2, 11);
+       ll.print();
+       ll.removeFirst();
        ll.print();
 
 
