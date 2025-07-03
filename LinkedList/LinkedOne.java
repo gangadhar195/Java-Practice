2db012a8375771a29c8e28b9c465cb0d69a99256
gangadhar195 <gangadhars9545@gmail.com>
2025-07-03 10:30:58 +0530

    search key

--- LinkedList/LinkedOne.java
+++ LinkedList/LinkedOne.java
@@ -62,11 +62,48 @@ public class LinkedOne {
         }else if(size==1){
             int val =head.data;
             head=tail=null;
+            size=0;
             return val;
         }
         int val =head.data;
         head=head.next;
+        size--;
         return val;
+    }
+
+    public int removeLast(){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
+
+    public int iterSearch(int key){
+        Node temp=head;
+        int i=0;
+        while (temp!=null) {
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return -1;
     }
 
     public static void print(){
@@ -82,18 +119,23 @@ public class LinkedOne {
         LinkedOne ll = new LinkedOne();
         ll.addFirst(2);
         // ll.print();
-    //     ll.addFirst(1);
+        ll.addFirst(1);
     //     // ll.print();
-    //     ll.addLast(3);
+        ll.addLast(3);
     //     // ll.print();
-    //     ll.addLast(4);
+        ll.addLast(4);
     //     ll.print();
     //     // ll.print();
     //    ll.add(2, 11);
        ll.print();
        ll.removeFirst();
        ll.print();
+       ll.removeLast();
+       ll.print();
+       System.out.println(ll.iterSearch(3));
+       System.out.println(ll.iterSearch(10));
 
+    System.out.println(ll.size);
 
         
     }
