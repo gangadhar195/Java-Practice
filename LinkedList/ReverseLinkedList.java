8428f5eb47fc2ffd17c4417937bef491cb3c98eb
gangadhar195 <gangadhars9545@gmail.com>
2025-07-04 06:57:33 +0530

    Practics LinkedList

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -9,8 +9,59 @@ public class ReverseLinkedList {
             this.data=data;
             this.next=null;
         }
-    }
+       
+        }
+         public static Node head;
+        public static Node tail;
+
+        public static void addFirst(int data){
+            Node newNode=new Node(data);
+            if(head==null){
+                head=tail=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+
+        public static void addLast(int data){
+            Node newNode=new Node(data);
+            if(head==null){
+                head=tail=newNode;
+                return;
+            }
+            tail.next=newNode;
+            tail=newNode;
+        }
+        public static void reverseNode(){
+            Node prev=null;
+            Node curr=head;
+            Node next;
+            while(curr!=null){
+                next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+            }
+            head=prev;
+        }
+        public static void print(){
+            Node temp=head;
+            while (temp!=null) {
+                System.out.print(temp.data+"->");
+                temp=temp.next;
+            }
+            System.out.println("null");
+        }
     public static void main(String args[]){
         ReverseLinkedList ll = new ReverseLinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.print();
+        ll.reverseNode();
+        ll.print();
+
     }
 }
