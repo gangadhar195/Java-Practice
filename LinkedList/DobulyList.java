ec76a48da5428038b7629ac86a175c6a231895cb
gangadhar195 <gangadhars9545@gmail.com>
2025-07-07 20:47:19 +0530

    DoublyLL

---
+++ LinkedList/DobulyList.java
@@ -0,0 +1,67 @@
+public class DobulyList {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static Node prev;
+    public static int size;
+
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public static int removeFrist(int data) {
+        if (head == null) {
+            head = tail = null;
+            return Integer.MAX_VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        DobulyList dll = new DobulyList();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.removeFrist(size));
+        dll.print();
+    }
+}
