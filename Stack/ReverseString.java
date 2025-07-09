bdda4370b18e52cc1e262db349f69e6999dae875
gangadhar195 <gangadhars9545@gmail.com>
2025-07-09 10:54:42 +0530

    Stack PushBottom and reverse

---
+++ Stack/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ReverseString {
+    public static String reverseString(String str){
+        Stack<Character> s=new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        str = result.toString();
+        return str;
+    }
+    public static void main(String args[]){
+        String str="abc";
+        System.out.println(reverseString(str));
+    }
+}
