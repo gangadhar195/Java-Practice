e3ed41843e49db41460b0f387c82d315f842aac2
gangadhar195 <gangadhars9545@gmail.com>
2025-07-12 00:58:29 +0530

    Stack Assignment

--- Stack/StackAss.java
+++ Stack/StackAss.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class StackAss {
      public static void main(String args[]){
         Stack s = new Stack<>();
+        int top[]=new int[3];
         s.push(1);
         s.push(2);
         s.push(3);
@@ -9,8 +10,12 @@ public class StackAss {
         System.out.println("Og Stack : ");
         while(!s.isEmpty()){
             System.out.print(s.pop()+" ");
+            s.push(top);
         }
-        
 
+        if(s.isEmpty()){
+            System.out.println(s.push(top));
+        }
+        System.out.println(s.peek());
     }
 }
