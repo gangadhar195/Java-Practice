4f900a55b1f1cc113e033f655aa587b1582a109b
gangadhar195 <gangadhars9545@gmail.com>
2025-07-20 11:23:13 +0530

    Parenthesis program

--- Stack/Parenthesis.java
+++ Stack/Parenthesis.java
@@ -28,9 +28,33 @@ public class Parenthesis {
             }
         
     }
-    public static void main(String args[]){
-        String str = "({}){[]}";
 
-        System.out.println(isValid(str));
+    public static boolean isDublicate(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch==')'){
+                int count=0;
+                while(s.peek() !='('){
+                    s.pop();
+                    count++;
+                }
+                if(count < 1){
+                    return true; // dublicate
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        // String str = "({}){[]}";
+
+        String str="((a+b))";
+        // System.out.println(isValid(str));
+        System.out.println(isDublicate(str));
     }
 }
