dc405784833825238c2341b0bff268901842bdb4
gangadhar195 <gangadhars9545@gmail.com>
2025-07-23 15:47:01 +0530

    Queues with Array

--- Stack/StackBasic.java
+++ Stack/StackBasic.java
@@ -40,8 +40,49 @@ public class StackBasic {
         }
         System.out.println("Maximum Reactangle : "+MaxReactangle);
     }
+
+    public static void MinHistogram(int arr[]){
+        int mincurr=0;
+        int nrl[] = new int[arr.length];
+        int nrr[] = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+        //large right
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nrr[i]=-1;
+            }else{
+                nrr[i]=s.peek();
+            }
+            s.push(i);
+        }
+        //heightest left
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nrl[i]=-1;
+            }else{
+                nrl[i]=s.peek();
+            }
+            s.push(i);
+        }
+        //currmin
+        for(int i=0;i<arr.length;i++){
+            int height = arr[i];
+            int width = nrr[i]-nrl[i]-1;
+            int currmin = height*width;
+            mincurr = Math.min(mincurr, currmin);
+        }
+        System.out.println("Min Histogram : "+ mincurr);
+    }
     public static void main(String args[]){
         int arr[] = {2,1,4,6,2,3};
-        findHistogram(arr);
+        // findHistogram(arr);
+        MinHistogram(arr);
+
     }
 }
