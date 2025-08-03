5bd2d7191f22caa7235bf49c5c5d06fc5d375eab
gangadhar195 <gangadhars9545@gmail.com>
2025-08-04 00:41:49 +0530

    Find Max length of chain

---
+++ Greedy Algo/ChainLenMax.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ChainLenMax {
+   public static void main(String args[]){
+    int pairs[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
+    Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
+    int chainlenth = 1;
+    int chainEnd = pairs[0][1];
+    for(int i=1;i<pairs.length;i++){
+        if(pairs[i][0]>chainEnd){
+            chainlenth++;
+            chainEnd=pairs[i][1];
+        }
+    }
+    System.out.println("Maximun length of chain : "+chainlenth);
+   } 
+}
