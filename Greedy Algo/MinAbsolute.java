b742fd92443f04654e31fe025ef1aa4a4581532e
gangadhar195 <gangadhars9545@gmail.com>
2025-08-03 23:58:34 +0530

    Find Min Absolute no  problem

---
+++ Greedy Algo/MinAbsolute.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MinAbsolute {
+    public static void main(String args[]){
+        int a[] = {4,1,8,7};
+        int b[]={2,3,6,5};
+
+        int minDiff=0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<a.length;i++){
+            minDiff += Math.abs(a[i]-b[i]);
+        }
+        System.out.println("Minimum Absolute Diffrenece : "+minDiff);
+    }
+}
