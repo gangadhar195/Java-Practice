dd042a51dbeadae0334ce5fee2e098290c615756
gangadhar195 <gangadhars9545@gmail.com>
2025-08-04 23:47:28 +0530

    some change

--- Greedy Algo/ChainLenMax.java
+++ Greedy Algo/ChainLenMax.java
@@ -5,12 +5,19 @@ public class ChainLenMax {
     Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
     int chainlenth = 1;
     int chainEnd = pairs[0][1];
+    ArrayList<Integer> ans=new ArrayList<>();
     for(int i=1;i<pairs.length;i++){
         if(pairs[i][0]>chainEnd){
             chainlenth++;
+            ans.add(pairs[i][0]);
+            ans.add(pairs[i][1]);
             chainEnd=pairs[i][1];
         }
     }
     System.out.println("Maximun length of chain : "+chainlenth);
+    for(int i=0;i<ans.size();i++){
+        System.out.print("A"+ans.get(i)+" ");
+    }
+    System.out.println();
    } 
 }
