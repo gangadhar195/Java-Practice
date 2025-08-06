dc1ecf3c806d73e33a734413735568de5b492ff1
gangadhar195 <gangadhars9545@gmail.com>
2025-08-06 17:22:21 +0530

    Binary Tree Basic

--- BinaryTree/BinaryBasic.java
+++ BinaryTree/BinaryBasic.java
@@ -27,12 +27,23 @@ public class BinaryBasic {
             newNode.right = builderTree(nodes);
             return newNode;
         }
+        public static void preorder(Node root){
+            if(root==null){
+                System.out.print(root+" ");
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
 
     public static void main(String args[]) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        Node root = BinaryTree.builderTree(nodes);
+        BinaryTree tree=new BinaryTree();
+        Node root = tree.builderTree(nodes);
         System.out.println("Root Node is: " + root.data);
+        tree.preorder(root);
     }
 }
 
