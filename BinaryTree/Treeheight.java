2b4d615c6adf0bbdd6077d4d4272d45020497433
gangadhar195 <gangadhars9545@gmail.com>
2025-08-07 10:14:09 +0530

    Find Daimeter and Height of tree

--- BinaryTree/Treeheight.java
+++ BinaryTree/Treeheight.java
@@ -57,14 +57,50 @@ public class Treeheight {
             int totalsum = leftsum + rightSum + root.data;
             return totalsum;
         }
+        public static int diameter2(Node root){
+            if(root == null){
+                return 0;
+            }
+            int leftdiameter = diameter2(root.left);
+            int lh = TreeH(root.left);
+            int rightdiameter = diameter2(root.right);
+            int rh = TreeH(root.right);
+            int selfdiameter = rh + lh+1;
+            int findlegth = Math.max(selfdiameter,Math.max(rightdiameter, leftdiameter));
+            return findlegth;
+        }
+        static class Info{
+            int diam;
+            int ht;
+            public Info(int diam,int ht){
+                this.diam=diam;
+                this.ht=ht;
+            }
+        }
+        public static Info daimeter(Node root){
+            if(root==null){
+                return new Info(0, 0);
+            }
+            Info leftdiam = daimeter(root.left);
+            Info rightdiam = daimeter(root.right);
+            int daim = Math.max(Math.max(leftdiam.diam, rightdiam.diam), leftdiam.ht+rightdiam.ht+1);
+            int ht = Math.max(leftdiam.ht, rightdiam.ht) +1;
+            return new Info(daim, ht);
+        }
+
     }
     public static void main(String args[]){
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6};
+        int nodes[]={1,2,4,7,-1,-1,8,-1,-1,5,-1,-1,3,-1,6,-1,9};
         BinaryTreeH tree = new BinaryTreeH();
         Node root = tree.builderTree(nodes);
        int height= tree.TreeH(root);
        System.out.println("Height : "+height);
        System.out.println("Total Node Count : "+tree.count(root));
        System.out.println("Total sum of Node : "+tree.sumofNode(root));
+       System.out.println("Total sum of Node : "+tree.diameter2(root));
+       System.out.println("Diameter of Tree : "+tree.daimeter(root).diam);
+       System.out.println("Height of Tree : "+tree.daimeter(root).ht);
+
+
     }
 }
