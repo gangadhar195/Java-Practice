6e25cfa2f7445f06d6e5899b957d604a21794961
gangadhar195 <gangadhars9545@gmail.com>
2025-08-08 21:50:40 +0530

    add transform tree

--- BinaryTree/Kthlevel.java
+++ BinaryTree/Kthlevel.java
@@ -20,6 +20,27 @@ public class Kthlevel{
         KthlevelNode(root.left,level+1,k);
         KthlevelNode(root.right,level+1,k);
     }
+
+    public static int transform(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftchild = transform(root.left);
+        int rightchild= transform(root.right);
+        int data= root.data;
+        int newleft = root.left==null?0:root.left.data;
+        int newright = root.right==null?0:root.right.data;
+        root.data= newleft + leftchild + newright + rightchild;
+        return data;
+    }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -29,6 +50,9 @@ public class Kthlevel{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
         int k=3;
-        KthlevelNode(root,1,k);
+        // KthlevelNode(root,1,k);
+        transform(root);
+        preorder(root);
+
     }
 }
