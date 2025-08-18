04df6d30e372931521cc6c7bd7ecbdba11e6afd7
gangadhar195 <gangadhars9545@gmail.com>
2025-08-18 15:56:50 +0530

    printRoot two Leaf tree

--- BTS/BTSBasic.java
+++ BTS/BTSBasic.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BTSBasic {
     static class Node{
         int data;
@@ -88,6 +90,24 @@ public class BTSBasic {
             printInRange(root.right, k1, k2);
         }
     }
+    public static void printpath(ArrayList<Integer> path){
+        for(int i=0;i<path.size();i++){
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.println("Null");
+    }
+    public static void printRoot2Leaf(Node root,ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left==null && root.right == null){
+            printpath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
     public static void main(String args[]){
         int values[] = {8,5,3,1,4,6,10,11,14};
         Node root=null;
@@ -105,7 +125,8 @@ public class BTSBasic {
     //    System.out.println();
     //    inorder(root);
     //    System.out.println(root);
-    printInRange(root, 5, 12);
+    // printInRange(root, 5, 12);
+    printRoot2Leaf(root,new ArrayList<>());
         
     }
 }
