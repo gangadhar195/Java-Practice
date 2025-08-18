65635e069c1b2e9e4446c97327c31f844ca4761a
gangadhar195 <gangadhars9545@gmail.com>
2025-08-18 13:55:10 +0530

    print in rage code

--- BTS/BTSBasic.java
+++ BTS/BTSBasic.java
@@ -72,8 +72,24 @@ public class BTSBasic {
         }
         return root;
     }
+    public static void printInRange(Node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
+        if(root.data >=k1 && root.data<=k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        }
+        else if(root.data< k1){
+            printInRange(root.left, k1, k2);
+        }
+        else{
+            printInRange(root.right, k1, k2);
+        }
+    }
     public static void main(String args[]){
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root=null;
         for(int i=0;i<values.length;i++){
            root= insert(root, values[i]);
@@ -85,9 +101,11 @@ public class BTSBasic {
         // }else{
         //     System.out.println("Not Found");
         // }
-       root =  delete(root, 3);
-       System.out.println();
-       inorder(root);
+    //    root =  delete(root, 3);
+    //    System.out.println();
+    //    inorder(root);
     //    System.out.println(root);
+    printInRange(root, 5, 12);
+        
     }
 }
