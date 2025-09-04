10947c77e2b29ea4391518a8e82b443fbbeb060e
gangadhar195 <gangadhars9545@gmail.com>
2025-09-04 20:07:57 +0530

    Heap Basic

---
+++ Heap/Basic.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+
+public class Basic {
+    public static void main(String args[]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>;
+        pq.add(3);
+        pq.add(5);
+        pq.add(1);
+        pq.add(2);
+        pq.add(4);
+    }
+}
