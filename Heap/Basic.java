d37605238f022bf6ba62d41bc98a625da4ee7337
gangadhar195 <gangadhars9545@gmail.com>
2025-09-05 13:13:09 +0530

    add Class object

--- Heap/Basic.java
+++ Heap/Basic.java
@@ -15,14 +15,14 @@ public class Basic {
         }
     }
     public static void main(String args[]){
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(3);
-        pq.add(5);
-        pq.add(1);
-        pq.add(2);
-        pq.add(4);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 20));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 1));
+        pq.add(new Student("E", 3));
         while(!pq.isEmpty()){
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name + "->"+pq.peek().rank);
             pq.remove();
         }
     }
