c9910d3eab157d2c26d323b30caf512e6a9bd3d4
gangadhar195 <gangadhars9545@gmail.com>
2025-09-05 13:05:39 +0530

    add Class object

--- Heap/Basic.java
+++ Heap/Basic.java
@@ -2,6 +2,18 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Basic {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name,int rank){
+            this.name=name;
+            this.rank=rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String args[]){
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         pq.add(3);
